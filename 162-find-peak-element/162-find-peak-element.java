c284f098db49148297d38139ac9dd73841dfbe16
Hasti Sutaria <69644003+HastiSutaria@users.noreply.github.com>
2022-02-18 11:11:35 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (32.23%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int i=0;
+        while(i<nums.length-1 && nums[i] <= nums[i+1])
+            i++;
+        return i;
+    }
+}
