8a16cdf1e77f838bc8f0965c38241c1db3378f02
Hasti Sutaria <69644003+HastiSutaria@users.noreply.github.com>
2022-03-02 19:17:40 +0530

    Time: 2 ms (45.06%), Space: 41.2 MB (46.24%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int count=0;
+        for(int i=0; i<t.length() && count<s.length(); i++){
+         if(s.charAt(count)==t.charAt(i))
+             count++;
+        }
+        return count==s.length();
+    }
+}
