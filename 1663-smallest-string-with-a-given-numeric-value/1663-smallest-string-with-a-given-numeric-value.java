43bdbf53adf8f350cb774758dddbdc3a60ece03b
Hasti Sutaria <69644003+HastiSutaria@users.noreply.github.com>
2022-03-23 00:12:46 +0530

    Time: 30 ms (52.38%), Space: 63.5 MB (50.79%) - LeetHub

---
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String getSmallestString(int n, int k) {
+        char[] arr = new char[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = 'a';
+        }
+        k-=n;
+        n--;
+        while(k>0) {
+            arr[n--] += Math.min(25, k);
+            k-=Math.min(25, k);
+        }
+        return String.valueOf(arr);
+    }
+}
