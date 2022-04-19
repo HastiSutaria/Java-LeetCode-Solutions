e9bc3a408c22aa7a2f6fbb051ee063c30c77c886
Hasti Sutaria <69644003+HastiSutaria@users.noreply.github.com>
2022-04-19 21:30:00 +0530

    Time: 2 ms (87.87%), Space: 42.1 MB (93.76%) - LeetHub

---
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -0,0 +1,30 @@
+class Solution {
+    private TreeNode first, second ,pre;
+    
+    public void recoverTree(TreeNode root) {
+        
+        
+        first = null; second = null; pre = null;
+        
+        inorder(root);         // calling the inorder helper
+        
+        //swap first and second value
+        int temp = first.val;
+        first.val = second.val; 
+        second.val = temp;
+    }
+    
+    private void inorder(TreeNode root){
+        if(root==null) return;
+        inorder(root.left);
+        
+        if(first==null && (pre==null ||pre.val>=root.val)){
+            first = pre;
+        }
+        if(first!=null && pre.val>=root.val){
+            second = root;
+        }
+        pre = root;
+        inorder(root.right);
+    }
+}
