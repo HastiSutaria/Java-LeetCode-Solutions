487eacff47d09bf1d1f37168d3f9c9e750490c38
Hasti Sutaria <69644003+HastiSutaria@users.noreply.github.com>
2022-04-22 22:28:50 +0530

    Time: 78 ms (27.31%), Space: 112.7 MB (5.02%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.java
+++ 706-design-hashmap/706-design-hashmap.java
@@ -1,19 +1,23 @@
-import java.util.*;  
 class MyHashMap {
-    int arr[];
+    
+    int size = (int)Math.pow(10,6)+1;  // define maximum limit of size of array (according to question constraint)
+    Integer[] arr;
+    
     public MyHashMap() {
-       arr = new int[10000000];
+        arr = new Integer[size];  // initialise array of that limit
     }
     
     public void put(int key, int value) {
-       arr[key]=value+1;
+        arr[key] = value;  // index of array is considered to be 'key' while arr[index] is the 'value'
     }
     
     public int get(int key) {
-        return arr[key]-1;
+        if(arr[key]==null)
+            return -1;
+        return arr[key]; // O(1) time complexity
     }
     
     public void remove(int key) {
-        arr[key]=0;
+        arr[key] = null;  // since array is of Integer type, it can store null also
     }
 }
