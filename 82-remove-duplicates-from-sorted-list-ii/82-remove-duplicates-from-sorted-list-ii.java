a58c589f20459d5a97d3a20faacf25018cca4fe0
Hasti Sutaria <69644003+HastiSutaria@users.noreply.github.com>
2022-05-02 23:04:06 +0530

    Time: 1 ms (76.36%), Space: 44.6 MB (5.99%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.java
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.java
@@ -1,30 +1,42 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode ansHead = new ListNode();
-        ListNode prev = ansHead, temp = head;;
-
-                while (temp != null) {
-                    ListNode current = temp;
-                    temp = temp.next;
-                    while (temp != null && current.val == temp.val) {
-                        temp = temp.next;
-                    }
-                    if (current.next == temp) {
-                        prev.next = current;
-                        prev = current;
-                        prev.next = null;
-                    }
-                }
-           return ansHead.next;
+        //Base condition no need to sort
+        if(head == null || head.next == null) {
+            return head;
+        }
+        
+        // Make a dummy node and connect its next to head
+        ListNode d = new ListNode(-1,head);
+        // Make dummy pointer that is currently pointing to d node
+        ListNode t = d;
+        
+        // Make curr node for travel through the LL
+        ListNode curr = head.next;
+        
+        // Loop till end of LL
+        while(curr != null) {
+            boolean flag = false; // boolean variable for checking conditions
+            // Loop till we are finding duplicate nodes
+            while(curr != null && curr.val == t.next.val) {
+                flag = true;
+                curr = curr.next;
+            }
+            
+            // if we have gone inside the loop that means we have loop and we have to point t's next to curr(because after the loop curr will be pointing to next distinct node)
+            if(flag) {
+                t.next = curr;
+            } else {
+                // if we have not gone inside the loop that means we have distinct node that means we have to increase the t node
+                t = t.next;
+            }
+            
+            // increase curr node in every step
+            if(curr != null) {
+                curr = curr.next;
+            }
+        }
+        
+        // return the head
+        return d.next;
     }
 }
